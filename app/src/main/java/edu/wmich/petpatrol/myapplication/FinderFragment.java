e4ca430d55b860f0e8854d03f3c779f2ee9b48c0
Moses <moses.s.troyer@gmail.com>
2016-03-30 19:06:01 -0400

    Put in a map asks for permissions api is a touch higher now because of the map

--- app/src/main/java/edu/wmich/petpatrol/myapplication/FinderFragment.java
+++ app/src/main/java/edu/wmich/petpatrol/myapplication/FinderFragment.java
@@ -6,18 +6,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.overlays.Marker;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 //this class is for maps with lost pets and reported pets pinned on them.
 public class FinderFragment extends Fragment{
 
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_find, container, false);
+
+        //put the map in the page
+        MapView map = (MapView) v.findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        //set up options
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        //init mapcontroller and give an initial point
+        IMapController mapController = map.getController();
+        mapController.setZoom(13);
+        GeoPoint startPoint = new GeoPoint(42.6549, -86.2032);
+        mapController.setCenter(startPoint);
+
+        //Place down the marker riiiight in the center and make it say cool things
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setTitle("Saugatuck");
+        startMarker.setSnippet("Coordinates: ");
+        GeoPoint location = startMarker.getPosition();
+        startMarker.setSubDescription("Lat: " + location.getLatitude() + ", Long: " + location.getLongitude());
+
+        map.getOverlays().add(startMarker);
 
         return v;
     }
--- app/src/main/java/edu/wmich/petpatrol/myapplication/MainActivity.java
+++ app/src/main/java/edu/wmich/petpatrol/myapplication/MainActivity.java
@@ -1,13 +1,40 @@
 package edu.wmich.petpatrol.myapplication;
 //main activity that holds all the fragments.
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //Moses was here 2016
 
 public class MainActivity extends SingleFragmentActivity {
 
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+
     @Override
     protected Fragment createFragment(){
+
+        //Ask for permissions, as done by the OSMDroid sample APP
+        List<String> permissions = new ArrayList<>();
+        String message = "Pet Patrol requires these permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nLocation to show user location.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
+
         return new HomeFragment();
     }
 }
