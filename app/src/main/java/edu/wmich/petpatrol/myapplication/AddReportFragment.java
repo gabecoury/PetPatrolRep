cbd5b80aecb3e1b9bddf37b33aa87ae7f92a57ba
andrewpeter95 <andrew.j.peter@wmich.edu>
2016-04-17 19:05:41 -0400

    Added Basic Form Input Submission (Partial & Local Only)
    - Event and Pet forms store input in local object variables
    
    - Date Picker added, only works with Event Start Date currently
    
    - No form validity checks
    
    - Only works for Events
    
    - Shows a toast message when event form submitted (MUST  fill out Start Date and Contact Number to prevent crash)

--- app/src/main/java/edu/wmich/petpatrol/myapplication/AddReportFragment.java
+++ app/src/main/java/edu/wmich/petpatrol/myapplication/AddReportFragment.java
@@ -1,22 +1,44 @@
 package edu.wmich.petpatrol.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Date;
 
 //this class if for reporting  found pets and lost pets
 public class AddReportFragment extends Fragment{
 
+    private static final int REQUEST_DATE = 0;
+    private static final String DIALOG_DATE = "DialogDate";
+
     private RadioGroup mRadioGroupReportType;
     private Button mPetSubmitButton;
     private Button mEventSubmitButton;
+    private Button mEventStartDateButton;
+    private Button mEventEndDateButton;
+    private Button mEventStartTimeButton;
+    private Button mEventEndTimeButton;
+    private DatePicker mEventStartDatePicker;
+    private DatePicker mEventEndDatePicker;
+    private Date mEventStartDate;
+    private Date mEventEndDate;
+    private Event mEvent = new Event();
+    private Pet mPet = new Pet();
+    private EditText EventNameEditText;
+    private EditText EventContactEditText;
+    private EditText EventDetailsEditText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +48,10 @@ public class AddReportFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_report, container, false);
+
+        EventNameEditText = (EditText) v.findViewById(R.id.editTextEventName);
+        EventContactEditText= (EditText) v.findViewById(R.id.editTextEventContact);
+        EventDetailsEditText = (EditText) v.findViewById(R.id.editTextEventDetails);
 
         final LinearLayout mLinearLayoutPet = (LinearLayout) v.findViewById(R.id.LinearLayoutPet);
         final LinearLayout mLinearLayoutEvent = (LinearLayout) v.findViewById(R.id.LinearLayoutEvent);
@@ -50,12 +76,91 @@ public class AddReportFragment extends Fragment{
             }
         });
 
+        mEventStartDateButton = (Button) v.findViewById(R.id.buttonEventStartDate);
+
+
+        mEventStartDateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mEventStartDate = new Date();
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mEventStartDate);
+                dialog.setTargetFragment(AddReportFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
+
+        mEventEndDateButton = (Button) v.findViewById(R.id.buttonEventEndDate);
+
+        mEventEndDateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        mEventStartTimeButton = (Button) v.findViewById(R.id.buttonEventStartTime);
+
+        mEventStartTimeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        mEventEndTimeButton = (Button) v.findViewById(R.id.buttonEventEndTime);
+
+        mEventEndTimeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+
         mPetSubmitButton = (Button) v.findViewById(R.id.buttonPetSubmit);
         mEventSubmitButton = (Button) v.findViewById(R.id.buttonEventSubmit);
 
         mPetSubmitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+
+
+                        RadioGroup PetStatusRadioGroup = (RadioGroup) v.findViewById(R.id.radioGroupPetStatus);
+                        EditText PetDescriptionEditText = (EditText) v.findViewById(R.id.editTextPetDetails);
+                        EditText PetNameEditText = (EditText) v.findViewById(R.id.editTextPetName);
+                        EditText PetContactEditText = (EditText) v.findViewById(R.id.editTextPetContact);
+                        EditText PetDetailsEditText = (EditText) v.findViewById(R.id.editTextPetDetails);
+
+                        switch(PetStatusRadioGroup.getCheckedRadioButtonId()) {
+                            case R.id.radioButtonPetLost:
+                                mPet.setFound(false);
+                                break;
+                            case R.id.radioButtonPetFound:
+                                mPet.setFound(true);
+                                break;
+                        }
+                        mPet.setPetDescription(PetDescriptionEditText.getText().toString());
+                        mPet.setPetName(PetNameEditText.getText().toString());
+                        mPet.setContactNumber(Integer.parseInt(PetContactEditText.getText().toString()));
+                        mPet.setDetails(PetDetailsEditText.getText().toString());
+
+                        // Insert Code for sending Pet Data to the server
+
+            }
+        });
+
+        mEventSubmitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+
+
+                mEvent.setEventName(EventNameEditText.getText().toString());
+                mEvent.setEventStartDateTime(mEventStartDate);
+                mEvent.setContactNumber(Integer.parseInt(EventContactEditText.getText().toString()));
+                mEvent.setDetails(EventDetailsEditText.getText().toString());
+
+                Toast.makeText(getContext(), "Event Created: " + mEvent.getEventName() + " will start on " + mEvent.getEventStartDateTime().toString() + ". Please call " + mEvent.getContactNumber() + " details: " + mEvent.getDetails(),Toast.LENGTH_LONG).show();
+
+                // Insert Code for sending Event Data to the server
             }
         });
 
@@ -63,4 +168,18 @@ public class AddReportFragment extends Fragment{
 
         return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mEventStartDate = date;
+        }
+    }
+
 }
