13b864ddaf8ebab13d662eef32dd57604cbff196
andrewpeter95 <andrew.j.peter@wmich.edu>
2016-04-17 20:36:00 -0400

    Updated Form Input Submission
    - Date Picker now works with both Event Start Date and Event End Date
    
    - Time Picker added, works with both Event Start Time and Event End Time
    
    - Still no form validity checks
    
    - Shows a toast message when event form submitted (MUST  fill in Contact Number to prevent crash)

--- app/src/main/java/edu/wmich/petpatrol/myapplication/AddReportFragment.java
+++ app/src/main/java/edu/wmich/petpatrol/myapplication/AddReportFragment.java
@@ -9,19 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 import java.util.Date;
 
 //this class if for reporting  found pets and lost pets
 public class AddReportFragment extends Fragment{
 
-    private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_START_DATE = 0;
+    private static final int REQUEST_END_DATE = 1;
+    private static final int REQUEST_START_TIME = 2;
+    private static final int REQUEST_END_TIME = 3;
     private static final String DIALOG_DATE = "DialogDate";
+    private static final String DIALOG_TIME = "DialogTime";
 
     private RadioGroup mRadioGroupReportType;
     private Button mPetSubmitButton;
@@ -30,10 +35,10 @@ public class AddReportFragment extends Fragment{
     private Button mEventEndDateButton;
     private Button mEventStartTimeButton;
     private Button mEventEndTimeButton;
-    private DatePicker mEventStartDatePicker;
-    private DatePicker mEventEndDatePicker;
     private Date mEventStartDate;
     private Date mEventEndDate;
+    private Date mEventStartTime;
+    private Date mEventEndTime;
     private Event mEvent = new Event();
     private Pet mPet = new Pet();
     private EditText EventNameEditText;
@@ -52,6 +57,10 @@ public class AddReportFragment extends Fragment{
         EventNameEditText = (EditText) v.findViewById(R.id.editTextEventName);
         EventContactEditText= (EditText) v.findViewById(R.id.editTextEventContact);
         EventDetailsEditText = (EditText) v.findViewById(R.id.editTextEventDetails);
+        mEventStartDate = new Date();
+        mEventEndDate = new Date();
+        mEventStartTime = new Date();
+        mEventEndTime = new Date();
 
         final LinearLayout mLinearLayoutPet = (LinearLayout) v.findViewById(R.id.LinearLayoutPet);
         final LinearLayout mLinearLayoutEvent = (LinearLayout) v.findViewById(R.id.LinearLayoutEvent);
@@ -81,11 +90,11 @@ public class AddReportFragment extends Fragment{
 
         mEventStartDateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mEventStartDate = new Date();
+
                 FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment
                         .newInstance(mEventStartDate);
-                dialog.setTargetFragment(AddReportFragment.this, REQUEST_DATE);
+                dialog.setTargetFragment(AddReportFragment.this, REQUEST_START_DATE);
                 dialog.show(manager, DIALOG_DATE);
             }
         });
@@ -95,6 +104,11 @@ public class AddReportFragment extends Fragment{
         mEventEndDateButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mEventEndDate);
+                dialog.setTargetFragment(AddReportFragment.this, REQUEST_END_DATE);
+                dialog.show(manager, DIALOG_DATE);
             }
         });
 
@@ -103,6 +117,11 @@ public class AddReportFragment extends Fragment{
         mEventStartTimeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                FragmentManager manager = getFragmentManager();
+                TimePickerFragment dialog = TimePickerFragment
+                        .newInstance(mEventStartTime);
+                dialog.setTargetFragment(AddReportFragment.this, REQUEST_START_TIME);
+                dialog.show(manager, DIALOG_TIME);
             }
         });
 
@@ -111,6 +130,11 @@ public class AddReportFragment extends Fragment{
         mEventEndTimeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                FragmentManager manager = getFragmentManager();
+                TimePickerFragment dialog = TimePickerFragment
+                        .newInstance(mEventEndTime);
+                dialog.setTargetFragment(AddReportFragment.this, REQUEST_END_TIME);
+                dialog.show(manager, DIALOG_TIME);
             }
         });
 
@@ -154,11 +178,20 @@ public class AddReportFragment extends Fragment{
 
 
                 mEvent.setEventName(EventNameEditText.getText().toString());
-                mEvent.setEventStartDateTime(mEventStartDate);
+//                Calendar cStart = Calendar.getInstance();
+//                cStart.setTimeInMillis(mEventStartDate.getTime() + mEventStartTime.getTime());
+//                Calendar cEnd = Calendar.getInstance();
+//                cEnd.setTimeInMillis(mEventEndDate.getTime() + mEventEndTime.getTime());
+//                mEvent.setEventStartDateTime(cStart.getTime());
+//                mEvent.setEventEndDateTime(cEnd.getTime());
+                Date combinedStart = new Date(mEventStartDate.getYear(), mEventStartDate.getMonth(), mEventStartDate.getDate(), mEventStartTime.getHours(),mEventStartTime.getMinutes());
+                Date combinedEnd = new Date(mEventEndDate.getYear(), mEventEndDate.getMonth(), mEventEndDate.getDate(), mEventEndTime.getHours(),mEventEndTime.getMinutes());
+                mEvent.setEventStartDateTime(combinedStart);
+                mEvent.setEventEndDateTime(combinedEnd);
                 mEvent.setContactNumber(Integer.parseInt(EventContactEditText.getText().toString()));
                 mEvent.setDetails(EventDetailsEditText.getText().toString());
 
-                Toast.makeText(getContext(), "Event Created: " + mEvent.getEventName() + " will start on " + mEvent.getEventStartDateTime().toString() + ". Please call " + mEvent.getContactNumber() + " details: " + mEvent.getDetails(),Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "Event Created: " + mEvent.getEventName() + " will start on " + mEvent.getEventStartDateTime().toString() + " and end on" + mEvent.getEventEndDateTime().toString() + ". Please call " + mEvent.getContactNumber() + " details: " + mEvent.getDetails(),Toast.LENGTH_LONG).show();
 
                 // Insert Code for sending Event Data to the server
             }
@@ -175,10 +208,29 @@ public class AddReportFragment extends Fragment{
             return;
         }
 
-        if (requestCode == REQUEST_DATE) {
+        if (requestCode == REQUEST_START_DATE) {
             Date date = (Date) data
                     .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mEventStartDate = date;
+
+
+        }
+        else if(requestCode == REQUEST_END_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mEventEndDate = date;
+        }
+        else if (requestCode == REQUEST_START_TIME) {
+            Date time = (Date) data
+                    .getSerializableExtra(TimePickerFragment.EXTRA_TIME);
+            mEventStartTime = time;
+
+
+        }
+        else if(requestCode == REQUEST_END_TIME) {
+            Date time = (Date) data
+                    .getSerializableExtra(TimePickerFragment.EXTRA_TIME);
+            mEventEndTime = time;
         }
     }
 
--- app/src/main/java/edu/wmich/petpatrol/myapplication/DatePickerFragment.java
+++ app/src/main/java/edu/wmich/petpatrol/myapplication/DatePickerFragment.java
@@ -18,31 +18,6 @@ import java.util.GregorianCalendar;
 /**
  * Created by Andrew on 4/17/2016.
  */
-//public class DatePickerFragment extends DialogFragment
-//        implements DatePickerDialog.OnDateSetListener {
-//
-//
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//// Use the current date as the default date in the picker
-//        final Calendar c = Calendar.getInstance();
-//        int year = c.get(Calendar.YEAR);
-//        int month = c.get(Calendar.MONTH);
-//        int day = c.get(Calendar.DAY_OF_MONTH);
-//
-//// Create a new instance of DatePickerDialog and return it
-//        return new DatePickerDialog(getActivity(), this, year, month, day);
-//    }
-//
-//    @Override
-//    public void onDateSet(DatePicker view, int year, int month, int day) {
-//        Calendar c = Calendar.getInstance();
-//        c.set(year, month, day);
-//
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        String formattedDate = sdf.format(c.getTime());
-//    }
-//}
 
 public class DatePickerFragment extends DialogFragment {
 
